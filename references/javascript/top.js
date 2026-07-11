95a8e4c974acb527a319857a8ed16657c09db090
klee123-gif <yiejintian@gmail.com>
2026-07-12 07:12:04 +0800

    Update top.js

--- references/javascript/top.js
+++ references/javascript/top.js
@@ -1,6 +1,7 @@
 const navi = document.querySelector(".navi");
 const arrow0 = document.querySelector(".arrow0");
 const arrow1 = document.querySelector(".arrow1");
+const arrow2 = document.querySelector(".arrow2");
 const icons = document.querySelector(".search0");
 const logo10 = document.querySelector(".logo10");
 const logo11 = document.querySelector(".logo11");
@@ -8,7 +9,7 @@ const logo11 = document.querySelector(".logo11");
 window.addEventListener("scroll", () => {
     const scrolled = window.scrollY > 650;
 
-    [navi, arrow0, arrow1, icons, logo10, logo11].forEach(element => {
+    [navi, arrow0, arrow1, arrow2, icons, logo10, logo11].forEach(element => {
         if (element) {
             element.classList.toggle("scrolled", scrolled);
         }
@@ -107,5 +108,4 @@ question4.addEventListener("click", () => {
         q4add2.classList.add('show');
     }
 });
-
 
